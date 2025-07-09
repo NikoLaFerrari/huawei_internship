eb63c7bf37ffeea43fdb9ad5537f66bde729ce65
Jagan Koip <106874502+NikoLaFerrari@users.noreply.github.com>
2025-07-09 17:20:25 +0800

    Update and rename Error - qwen3-7b to Error-qwen3-7b.py

--- Error-qwen3-7b.py
+++ Error-qwen3-7b.py
@@ -1,3 +1,4 @@
+'''
 Error executing job with overrides: []
 Traceback (most recent call last):
   File "/models/k0050048751/MindSpeed-RL-master/cli/train_grpo.py", line 574, in <module>
@@ -42,3 +43,50 @@ ray.exceptions.RayTaskError(KeyError): ray::train() (pid=2182, ip=172.16.4.110)
 KeyError: 'input_ids'
 /usr/local/python3.10.16/lib/python3.10/tempfile.py:869: ResourceWarning: Implicitly cleaning up <TemporaryDirectory '/tmp/tmpv40n0lke'>
   _warnings.warn(warn_message, ResourceWarning)
+
+
+
+
+import os
+import shutil
+import struct
+import glob
+import re
+from enum import Enum
+from functools import lru_cache
+from abc import ABC, abstractmethod
+from itertools import accumulate
+from types import TracebackType
+from typing import Optional, Tuple, Type, Union, List
+
+import torch
+import numpy
+
+_INDEX_HEADER = b"MMIDIDX\x00\x00"
+'''
+
+def get_packed_indexed_dataset(data_prefix: str, filter_length: Optional[int] = None):
+    index_dataset_name = f"{data_prefix}_packed_*_document*"
+    names = glob.glob(index_dataset_name)
+    template = f"{data_prefix}_packed_(.*)_document(.*)"
+    all_field = set()
+    for name in names:
+        fields = re.match(template, name)
+        all_field.add(fields.group(1))
+    packed_dataset = dict()
+
+    for field in all_field:
+        # We only do filter for input_ids when filter_length is specified
+        max_len = filter_length if filter_length and field == 'input_ids' else None
+        packed_dataset[field] = IndexedDataset(f"{data_prefix}_packed_{field}_document", max_len=max_len)
+
+    if filter_length:
+        filter_mask = packed_dataset['input_ids'].get_filter_mask()
+        for field in packed_dataset:
+            packed_dataset[field].do_filter(filter_mask)
+
+    combine_dataset = CombinedDataset(packed_dataset)
+    return combine_dataset
+
+
+
